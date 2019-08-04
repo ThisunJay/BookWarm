dc253976c61b8eed09714bbdad33bf3c01bc24b6
ThisunJay <jayamaduthisun@gmail.com>
2019-08-04 16:27:39 +0530

    Thisun last update

--- app/src/main/java/com/example/student/bookworm/ViewBook.java
+++ app/src/main/java/com/example/student/bookworm/ViewBook.java
@@ -1,7 +1,9 @@
 package com.example.student.bookworm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewBook extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ViewBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_book);
+    }
+
+    public void deleteReview(View view){
+        Intent intent = new Intent(ViewBook.this, BooksMain.class);
+        startActivity(intent);
+    }
+
+    public void updateReview(View view){
+        Intent intent = new Intent(ViewBook.this, updateReview.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/student/bookworm/updateReview.java
@@ -0,0 +1,20 @@
+package com.example.student.bookworm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class updateReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_review);
+    }
+
+    public void updateButton(View view){
+        Intent intent = new Intent(updateReview.this, ViewBook.class);
+        startActivity(intent);
+    }
+}
