51fc0c02d0a63c69975ba246f430829e001493bc
Adithya <agrac98@gmail.com>
2019-09-05 16:37:53 +0530

    DB eka

--- app/src/main/java/com/example/student/bookworm/AddReadAct.java
+++ app/src/main/java/com/example/student/bookworm/AddReadAct.java
@@ -4,13 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddReadAct extends AppCompatActivity {
+    DatabaseHelper adidb;
+    EditText editName,editAuthor,editFrom,editTill,editGenre;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_read);
+        adidb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.ttin7);
+        editAuthor = (EditText) findViewById(R.id.ttin8);
+        editFrom = (EditText) findViewById(R.id.Aet3);
+        editTill = (EditText) findViewById(R.id.Aet6);
+        editGenre = (EditText) findViewById(R.id.AedG);
+        btnAdd = (Button) findViewById(R.id.Ab1);
+
+        AddData();
+    }
+
+    public void AddData(){
+        btnAdd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean isInserted = adidb.insertData(editName.getText().toString(),
+                                editAuthor.getText().toString(),
+                                editFrom.getText().toString(),
+                                editTill.getText().toString(),
+                                editGenre.getText().toString()
+                        );
+                        if(isInserted == true) {
+                            Toast.makeText(AddReadAct.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(AddReadAct.this,"Data Not Inserted",Toast.LENGTH_LONG).show();
+                        }
+                    };
+                }
+        );
     }
 
     public void onAddR(View view){
